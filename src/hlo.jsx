f376cdb902cf69389bc191c88edea4f59c5c0eca
Jede Jagadeesh <jagadeeshjade490@gmail.com>
2025-10-22 06:06:34 +0530

    Initial commit

--- src/hlo.jsx
+++ src/hlo.jsx
@@ -13,7 +13,7 @@ function Home() {
       <div className="hero-content">
         <h2>🌿 Protect Nature, Preserve Life 🌿</h2>
         <p>
-          Join <strong>TreeBridge</strong> in connecting communities with nature.
+          Join <strong>EcoConnect</strong> in connecting communities with nature.
           Plant trees, protect animals, and restore ecosystems for a sustainable future.
         </p>
         <Link to="/project">
@@ -35,11 +35,11 @@ export default function Hlo() {
       {/* Navbar */}
       <nav className="navbar">
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/project">Project</Link></li>
-          <li><Link to="/community">Community 🌿</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/" style={{color:'black',fontSize:'25px'}}>Home</Link></li>
+          <li><Link to="/about" style={{color:'black',fontSize:'25px'}}>About</Link></li>
+          <li><Link to="/project" style={{color:'black',fontSize:'25px'}}>Project</Link></li>
+          <li><Link to="/community" style={{color:'black',fontSize:'25px'}}>EcoUnity🌿</Link></li>
+          <li><Link to="/contact" style={{color:'black',fontSize:'25px'}}>Contact</Link></li>
         </ul>
       </nav>
 
